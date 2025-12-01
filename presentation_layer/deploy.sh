6ad132ecee8b10fc19dbe84e02812591d0213535
Daniel Bernal <163784010+FormalIngenieroniel@users.noreply.github.com>
2025-12-01 17:02:16 -0500

    Update deploy.sh

--- presentation_layer/deploy.sh
+++ presentation_layer/deploy.sh
@@ -1,42 +1,40 @@
 #!/bin/bash
 
-# Definimos nombres para facilitar cambios futuros
+# En este archivo se define el proceso paa poder correr la aplicacion web.
+
 IMAGE_NAME="web-app"
 CONTAINER_NAME="web_container"
 PORT="8501"
 
-# 1. LIMPIEZA DE CONTENEDORES
-# Verificamos si existe un contenedor con ese nombre (corriendo o detenido)
+# Primero se realiza una comprobacion para verificar si existe un contenedor con el mismo nombre 
+# ya sea que este corriendo o este detenido. En caso de que exista se detiene y se elimina para
+# evitar problemas.
+
 if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
     echo "Deteniendo y eliminando contenedor existente: $CONTAINER_NAME..."
-    # Detener (ignora error si ya está detenido)
     docker stop $CONTAINER_NAME >/dev/null 2>&1
-    # Eliminar
     docker rm $CONTAINER_NAME
     echo "✅ Contenedor eliminado."
 else
     echo "ℹ️ No se encontró contenedor previo con nombre $CONTAINER_NAME."
 fi
 
-# 2. LIMPIEZA DE IMÁGENES
-# Verificamos si existe la imagen para borrarla y liberar espacio antes del build
+# De la misma manera se hace una verificamos de si existe una imagen previa, en caso de existir
+# se borra y se libera el espacio antes de realizar el build.
+
 if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" != "" ]]; then
     echo "🧹 Eliminando imagen antigua ($IMAGE_NAME) para liberar memoria..."
     docker rmi $IMAGE_NAME
     echo "✅ Imagen eliminada."
 fi
 
-# 3. CONSTRUCCIÓN (BUILD)
-echo "🏗️ Construyendo nueva imagen..."
-# Nota: Asegúrate de que tu archivo se llame 'Dockerfile' y no 'Dockerfile.txt'
-if [ -f "Dockerfile.txt" ]; then
-    echo "⚠️ Detectado Dockerfile.txt, renombrando a Dockerfile..."
-    mv Dockerfile.txt Dockerfile
-fi
+# Se construye la imagen y se verifica del exito de esta operacion. De ser exitosa, se levanta
+# la imagen en el puerto 8501. En la instancia EC2 se permite el trafico utilizando 0.0.0.0
+# en el security group para poder acceder con la direccion DNS publica.
 
+echo "🏗️ Construyendo nueva imagen..."
 docker build -t $IMAGE_NAME .
 
-# Verificamos si el build fue exitoso
 if [ $? -eq 0 ]; then
     echo "✅ Build exitoso."
 else
@@ -44,9 +42,6 @@ else
     exit 1
 fi
 
-# 4. EJECUCIÓN (RUN)
-echo "🔥 Levantando contenedor..."
+echo "Levantando contenedor..."
 docker run -d --name $CONTAINER_NAME -p $PORT:8501 $IMAGE_NAME
-
-
-echo "🌍 Tu app disponible en: http://localhost:$PORT"
+echo "🌍 Lu app esta disponible en: http://localhost:$PORT"

5db8dce793f9620cbd1cd4ef0cf8f4b5e9fe1a19
Daniel Bernal <163784010+FormalIngenieroniel@users.noreply.github.com>
2025-12-01 11:50:22 -0500

    Update main.py

--- logic_layer/main.py
+++ logic_layer/main.py
@@ -11,13 +11,20 @@ from agents import app_graph
 app = FastAPI()
 
 # Se utiliza pydantic para asegurarnos que la pregunta del usuario siempre sera un str y no
-# hayan problemas al ingresar otro tipo de dato
+# hayan problemas al ingresar otro tipo de dato, ademas especifica que el campo se llame
+# 'question'
 
 class QueryRequest(BaseModel):
     question: str
 
 # Se define el endpoint que se utilizara para hacer la pregunta a los agentes, se comienza
-# con la definicion de la base de la peticion
+# con la definicion de la base de la peticion siendo esta tipo post, que al ser llamada,
+# primero creara la base de la solicitud que se le pasara a cada agente en el "initial_state".
+# Esta contendra la pregunta, los diferentes analisis y la sintesis, que seran los resultados
+# a mostrar en la web. 
+# Se hace el llamado al grafo de procesamiento de los agente que esta en el archivo "agents.py" 
+# y almacena la respuesta, con la estructura mencionada anteriormente, en la variable 
+# "final_state". La peticion retorna los pensamientos de cada agente y la sintesis.
 
 @app.post("/ask")
 async def ask_agent(request: QueryRequest):
@@ -30,11 +37,12 @@ async def ask_agent(request: QueryRequest):
             "final_synthesis": ""
         }
         
-        # Ejecutamos el grafo
-        # invoke devuelve el estado final después de pasar por todos los nodos
         final_state = app_graph.invoke(initial_state)
+
+        # Para facilidad a la hora de trabajar con los datos, se extrae la sintesis y los
+        # diferentes analisis de los agentes por separado en las variables "synthesis" y
+        # "logs" respectivamente
         
-        # Extraemos resultados del estado final
         synthesis = final_state.get("final_synthesis", "Error generando síntesis.")
         logs = final_state.get("analysis_logs", [])
         
@@ -49,6 +57,8 @@ async def ask_agent(request: QueryRequest):
         print(f"❌ Error Crítico en Logic Layer: {e}")
         raise HTTPException(status_code=500, detail=str(e))
 
-if __name__ == "__main__":
+# Se define la ejecucion que subira la API en un servidor que estara escuchando las peticiones 
+# localmente en el puerto 5000
 
+if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000)
